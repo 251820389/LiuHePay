b490fc1aba21c1a9c58d78fca21b19c7f508637e
251820389 <251820389@qq.com>
2016-08-04 11:41:41 +0800

    test

--- src/main/java/com/liuhepay/cuppayment/util/ConfigUtil.java
+++ src/main/java/com/liuhepay/cuppayment/util/ConfigUtil.java
@@ -7,6 +7,7 @@ public class ConfigUtil {
 	public static final int ALIPAYPAY = 3;
 	public static final int SALE = 10001;
 	public static final int PREAUTH = 10101;
+	public static final int PREAUTHCOMPLETE = 10102;
 
 	// 收款金额转换
 	public static String covertPoint(String price) {
